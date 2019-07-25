46dcf551067d00c33c213f715bc6ac43a5523902
Alejandro Vivas <ajvivas86@gmail.com>
2019-07-24 23:40:06 -0400

    Ajustes

--- src/assets/js/index.js
+++ src/assets/js/index.js
@@ -1,23 +1,19 @@
-import 'pageable/dist/pageable.min.css'
-import '../stylesheets/main.scss'
+import "font-awesome/css/font-awesome.css";
+import "../stylesheets/main.scss";
 
-const Pageable = require('pageable')
+import "./smooth";
+import "./navbar";
 
-const WOW = require('wow.js')
+const WOW = require("wow.js");
 
 $(document).ready(() => {
-
-
-  //new Pageable("#container");
-
   const wow = new WOW({
-    boxClass: 'wow', // default
-    animateClass: 'sr--anim', // default
+    boxClass: "wow", // default
+    animateClass: "sr--anim", // default
     offset: 150, // default
     mobile: true, // default
     live: true // default
-  })
+  });
 
   wow.init();
-
 });
